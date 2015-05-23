7635f9e238762fe1b4536daca2cdf6bb962931be
Patrick Quinn-Graham <make-contact@pftqg.com>
2015-05-23 18:38:00 +1000

    v1.0.1

--- bin/router.js
+++ bin/router.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
-require('babel/register');
+require('babel/register')({
+  // We need to do this so babel actually parses our files
+  only: /frontendserver\/lib\/.*/
+});
 require('../lib/router.js');
